a2401b7fc731dc1cf814b8d6f8796b38a9932710
marcelacarrasco <marcela1carrasco@gmail.com>
2021-08-18 17:47:39 -0600

    added a search engine

--- src/weather.js
+++ src/weather.js
@@ -49,7 +49,19 @@ function displayTemperature(response) {
   currentIconElement.setAttribute("atl", response.data.weather[0].description);
 }
 
-let apiKey = "06c815873f95de2eb641338b25fec569";
-let city = "Aspen";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "06c815873f95de2eb641338b25fec569";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Aspen");
